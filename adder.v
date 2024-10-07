0dd95f82d846f2195d0d6151134c3179707ccc1e
oguri <120602715@sjtu.edu.cn>
2024-10-07 22:15:06 +0800

    repair the error

--- adder.v
+++ adder.v
@@ -28,14 +28,11 @@ module adder_4(
 
     assign c[0] = carry_in;
     assign c[1] = g[0] | (p[0] & carry_in);
-    assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & carry_in);
-    assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & carry_in);
-    assign carry_out = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & carry_in);
-
+    assign c[2] = g[1] | (p[1] & g[0]) | (p[0] & carry_in);
+    assign c[3] = g[2] | (p[2] & g[1]) | (p[1] & (g[0] | (p[0] & carry_in)));
+    assign carry_out = g[3] | (p[3]&(g[2]|(p[2]&(g[1]|(p[1]&(g[0]|(p[0]&carry_in)))))));
     assign sum = p ^ c[3:0];
-
-
-
+	
 endmodule
 
 module adder(
